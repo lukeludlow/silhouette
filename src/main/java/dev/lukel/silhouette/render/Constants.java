f56b54fedf734b6cec18877cbc575eac407214ae
lukeludlow <lukeludlow@live.com>
2022-01-04 16:19:17 -0700

    freeze

--- src/main/java/dev/lukel/silhouette/render/Constants.java
+++
@@ -1,6 +0,0 @@
-package dev.lukel.silhouette.render;
-
-public class Constants {
-    public static final int HIDE_RGBA = 69;
-    public static final double DISPLAY_GAMERTAGS_DISTANCE_LIMIT = 4096D;
-}
